931adbb1349210cf43fc482d4fd7583ac87f3ba8
Sudipto Das <dassudipto200@gmail.com>
2025-06-14 23:35:40 +0530

    changing docker workflow and retrying

--- frontend/src/app/dashboard/admin/page.tsx
+++ frontend/src/app/dashboard/admin/page.tsx
@@ -1,11 +1,9 @@
-import LineChart from "../../../../archive/(admin)/(others-pages)/(chart)/line-chart/page";
 import BarChartOne from "@/components/charts/bar/BarChartOne";
 import { BarChart } from "lucide-react";
 
 export default function AdminDashboard(){
     return(
         <div className="">
-            <LineChart/>
             <BarChartOne/>
         </div>
     )
